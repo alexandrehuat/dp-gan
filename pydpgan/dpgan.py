3488ef8929d03f70f6ebdd66cf007a357b58c219
Alexandre Huat <alexandre.huat@gmail.com>
2018-03-09 00:08:19 +0100

    non private gan ready, needs test

--- pydpgan/dpgan.py
+++ pydpgan/dpgan.py
@@ -18,33 +18,41 @@ class BasicDPGAN(DPGAN):
         super().__init__(generator, discriminator)
 
 
-    def train(self, X, Z=np.random.uniform, epochs=100, lam=0.01, n_critic=100, batch_size=100, optimizer=AdamOptimizer(), C=1, sigma=1, eps0=10, delta0=1e-05):
+    def train(self, X, Z=np.random.uniform, epochs=100, lam=0.01, n_critic=100, batch_size=100, optimizer=AdamOptimizer(), C=1, sigma=1, eps0=10, delta0=1e-05, tol=1e-6):
         # Building the training tensor flow
-        x = tf.placeholder(TF_FLOAT, self.discriminator.input_shape)
-        rho = tf.random_uniform((1,), 0, 1, TF_FLOAT)
-        z = tf.placeholder(TF_FLOAT, self.generator.input_shape)
+        x = tf.placeholder(TF_FLOAT, (batch_size,) + self.discriminator.input_shape)
+        rho = tf.random_uniform((batch_size,), 0, 1, TF_FLOAT)
+        z = tf.placeholder(TF_FLOAT, (batch_size,) + self.generator.input_shape)
         x_hat = tf.multiply(rho, x) + tf.multiply(tf.constant(1) - rho, self.generator(z))
-        loss_i = self.discriminator(self.generator(z)) - self.discriminator(x) + tf.multiply(tf.constant(lam), tf.square(tf.norm(tf.gradients(self.discriminator(x_hat), x_hat)) - tf.constant(1)))  # differentiable by w
-        grad_i = tf.gradients(loss_i, self.discriminator.weights)
-        ksi = tf.random_normal(grad_i.shape, 0, tf.multiply((sigma * C) ** 2, tf.eye(*grad_i.shape)))
-        grad_i = grad_i / tf.maximum(tf.constant(1), tf.norm(grad_i) / tf.constant(C)) + tf.constant(ksi)
+        D_loss = self.discriminator(self.generator(z)) - self.discriminator(x) + tf.multiply(tf.constant(lam), tf.square(tf.norm(tf.gradients(self.discriminator(x_hat), x_hat)) - tf.constant(1)))  # differentiable by w
+        D_grad = tf.gradients(D_loss, self.discriminator.weights)
+        ksi = tf.random_normal(D_grad.shape, 0, (sigma * C) ** 2)
+        grad = tf.divide(D_grad, tf.maximum(tf.constant(1), tf.divide(tf.norm(D_grad), tf.constant(C)))) + ksi
+        train_D = optimizer.apply_gradients((tf.reduce_mean(grad, axis=0), self.discriminator.weights))
+
+        G_loss = tf.reduce_mean(-self.discriminator(self.generator(z)), axis=0)
+        train_G = optimizer.minimize((G_loss, self.generator.weights))
 
         # Initializing the local variables
-        e = 0
         delta = 0
+        init_all = tf.initialize_all_variables()
 
         # Training loop
         with tf.Session() as sess:
-            while e < epochs and delta <= delta0:  # and the generator has not converged
+            sess.run(init_all)
+            for e in range(epochs):
                 for t in range(n_critic):
-                    batch_grads = []
-                    for i in range(batch_size):
-                        batch_grads.append(sess.run(grad_i, {x: X[rdm.choice(X.shape[0]), :], z: Z()}))
-
                     # Updating the privacy accountant
                     # ...
 
                     # Updating the discriminator
-                    # ...
+                    sess.run(train_D, {x: X[rdm.choice(X.shape[0], batch_size), :], z: Z(size=batch_size)})
 
-                e += 1
+                # Updating the generator
+                theta_old = self.generator.weights
+                sess.run(train_G, {z: Z(size=batch_size)})
+
+                # Stopping criterias
+                G_convergence = sess.run(tf.norm(self.generator.weights - theta_old) <= tol)
+                if delta <= delta0 or G_convergence:
+                    break
